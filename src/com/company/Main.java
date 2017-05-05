7208a7df34065cd02247d019982c838d071829c5
MichaelTrap <m.d.kouzin@gmail.com>
2017-05-05 22:23:03 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,30 @@
 package com.company;
 
-import com.company.card.Card;
+
 import com.company.deck.Deck;
 import com.company.deck.DeckController;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        Deck deck = new Deck("JavaMTG/src/com/company/files");
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
 
-        System.out.println("Колода готова");
-        System.out.println();
-        
-
-        for(int i = 0 ; i < deck.getCards().size();i++){
-            System.out.println(deck.getCardFromTop().getCardname()) ;
-
+        for(int i=0;i<5; i++){
+           deck.add(scanner.nextLine());
         }
 
+        DeckController deckController = new DeckController(deck);
+
+        System.out.println("Колода готова");
+
+        deckController.shuffle(deck);
+
+        deckController.cardPresent(1,deck);
+        }
     }
-}
